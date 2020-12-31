9af2ee05146f0df79d8a90ca4926caf42017e96c
anubhavjana <anubhav.jana@gmail.com>
2020-12-31 22:30:58 +0530

    Update local_settings.py

--- nursery_website/local_settings.py
+++ nursery_website/local_settings.py
@@ -1,5 +1,5 @@
 import os
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file)))
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
